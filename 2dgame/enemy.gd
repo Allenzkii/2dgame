c15f39aee32e1a517d46858360d1f3a71295f53d
Allen Atanante <allenmiguelatanante@gmail.com>
2025-06-01 00:49:11 +0800

    Update enemy.gd

--- 2dgame/enemy.gd
+++ 2dgame/enemy.gd
@@ -3,6 +3,11 @@ extends CharacterBody2D
 var speed = 40
 var player_chase = false
 var player = null
+var is_dead = false
+
+var knockback_velocity = Vector2.ZERO
+var knockback_force = 400
+var knockback_decay = 800
 
 var health = 100
 var player_inattack_zone = false
@@ -10,6 +15,7 @@ var can_take_damage = true
 
 func _physics_process(delta):
 	deal_with_damage()
+	knockback(delta)
 	
 	if player_chase:
 		position += (player.position - position)/speed
@@ -45,7 +51,7 @@ func _on_detection_area_body_exited(body: Node2D) -> void:
 
 func deal_with_damage():
 	if player_inattack_zone and Global.player_current_attack == true:
-		if can_take_damage == true:
+		if can_take_damage == true and not is_dead:
 			health = health - 40
 			$Take_dmg_cooldown.start()
 			can_take_damage = false
@@ -53,7 +59,17 @@ func deal_with_damage():
 			if health <= 0:
 				$AnimatedSprite2D.play("dead")
 				self.queue_free()
-
+func knockback(delta):
+	if knockback_velocity.length() > 0.1:
+		velocity = knockback_velocity
+		knockback_velocity = knockback_velocity.move_toward(Vector2.ZERO, knockback_decay * delta)
+		move_and_slide()
+	else:
+		velocity = Vector2.ZERO
+	
+func apply_knockback(from_position: Vector2) -> void:
+	var direction: Vector2 = (global_position - from_position).normalized()
+	knockback_velocity = direction * knockback_force
 
 func _on_take_dmg_cooldown_timeout() -> void:
 	can_take_damage =true
